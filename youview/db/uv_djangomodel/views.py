423b66d25815f69f20f6639d8055cd52e10e1c87
kbnskee <gt.karlkevinddomingo@gmail.com>
2024-04-13 21:58:40 +0800

    updated uv_djangomodel view

--- youview/db/uv_djangomodel/views.py
+++ youview/db/uv_djangomodel/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from youview.db.djangomodel.models import Log
+from youview.db.uv_djangomodel.models import Log
 
 def create_error_log():
     Log.objects.create(remarks="CREATE")
